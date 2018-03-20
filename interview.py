8eeae57fdaa2b341f1c9e3c39118cb330541c017
Justin Park <justin.park@hpe.com>
2018-03-20 12:46:30 -0500

    Add interview.py skeleton

---
+++ interview.py
@@ -0,0 +1,7 @@
+
+def is_palindrome(n):
+    pass
+
+if __init__ == '__main__':
+    numbers_to_check = [1, 35, 12321, 565, 151]
+    # Cycle through numbers to check and print 'X is a palindrome' or 'X is not a palindrome'
